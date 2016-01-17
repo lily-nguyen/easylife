20e0543c6ece60a7cf9cb1d00e501b19fc61f4c7
Huyen.Nguyen <huyen.nguyen@amblique.com>
2016-01-17 17:26:00 +0700

    re-organization

--- miningPage.js
+++ miningPage.js
@@ -1,7 +1,7 @@
 function MingingNameProduct() {
-	alert($(".product-card__name-wrap").length);
-	//var _productNameList = $(node).find(".product-card__name-wrap");
-	//alert(_productNameList.length);
+	//alert($(".product-card__name-wrap").length);
+	var _productNameList = $(".product-card__name-wrap");
+	alert(_productNameList.length);
     return "";
 }
 
