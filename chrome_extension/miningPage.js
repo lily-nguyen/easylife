41521d83354cd73069d4894af942841de851aa19
Huyen.Nguyen <huyen.nguyen@amblique.com>
2016-02-22 20:23:48 +0700

    get data from lazada - simple example

--- chrome_extension/miningPage.js
+++ chrome_extension/miningPage.js
@@ -1,7 +1,22 @@
 function MingingNameProduct() {
 	//alert($(".product-card__name-wrap").length);
 	var _productNameList = $(".product-card__name-wrap");
-	alert(_productNameList.length);
+	var arrayProduct = [];
+	var nameP = undefined;
+	for (var i = 0; i < _productNameList.length; i++) {
+		nameP = _productNameList[i].firstElementChild.title;
+		if (nameP) {
+			arrayProduct.push(nameP);
+		} else {
+			nameP = _productNameList[i].firstElementChild.textContent;
+			if (nameP && (!nameP.endsWith('...'))){
+				arrayProduct.push(nameP);
+			}
+			
+		}
+			
+	}
+	alert(arrayProduct);
     return "";
 }
 
